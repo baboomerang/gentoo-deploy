3cf389c436346c7f36b80558be3b9ba5dad98777
baboomerang <irodrigue101@gmail.com>
2021-04-23 00:02:32 -0400

    Latest update

--- gentoo-deploy.sh
+++ gentoo-deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-###########################
-#
-# Gentoo Deploy Script
-# Usage: ./gentoo-deploy.sh
-#
+################################
+#			                   #
+# Gentoo Deploy Script	       #
+# Usage: ./gentoo-deploy.sh    #
+#			                   #
 ################################
 
 SCRIPT=`realpath $0`
 ROOT="/mnt/gentoo"
-BOOT="$ROOT/boot"
-HOME="$ROOT/home"
+BOOT="${ROOT}/boot"
+HOME="${ROOT}/home"
 
 TFTP=192.168.0.3
 MIRROR="http://distfiles.gentoo.org"
@@ -25,120 +25,116 @@ PACKAGEKEYWORDS="/etc/portage/package.keywords"
 
 MAKECFLAGS='-march=native -O3 -pipe'
 MAKEOPTS="-j$(expr `nproc` + 1)"
-MAKEUSE='-bindist udev'
+MAKEUSE='-bindist -consolekit -webkit -vulkan -vaapi -vdpau -opencl -bluetooth -kde udev threads alsa pulseaudio mpeg mp3 flac aac lame midi ogg vorbis x264 xvid win32codecs real png jpeg jpeg2k raw gif svg tiff opengl bash bash-completion i3 vim vim-syntax git dbus qt4 cairo gtk unicode fontconfig truetype wifi laptop acpi lm_sensors dvd dvdr cdr cdrom pam policykit X dhcpcd logrotate python3_7 python3_9'
 MAKEPYTHON='python2_7 python3_3'
 MAKEINPUTDEVICES='evdev'
 MAKEVIDEOCARDS='intel i965'
 MAKELINGUAS='en_US en'
 
-PROFILE="desktop/kde/systemd"
-KERNEL_MODULES=''
-CLEAN="tr -d '\040\011\012\015'"
-
-
 install() {
-
-    # Open stderr and forward errors to a log file
-    exec 3>&2
-    exec 2> >(tee "$SCRIPT.log" >&2)
-
-    # Create initial folders, we will mount block devices to these folders
-    mkdir -p $ROOT/boot
-    mkdir -p $ROOT/home
-
     # Show all disks for the user
     lsblk
 
-    # Prompt user and save user input
-    local DISK
-    read -p "Which disk should be erased?(i.e. /dev/sdj) BACKUP ALL DATA \
-             BEFORE TYPING A DISK NAME YOU HAVE BEEN WARNED:" $DISK
+    # Prompt user for input and save the input
+    local disk
 
-    SECONDS=60
-    while [ ${SECONDS} -ge 1 ]; do
-        printf "\r!WARNING! BLOCK DEVICE:$DISK WILL BE ERASED IN %2d SECONDS....\n
-                PLEASE MAKE SURE THIS IS THE CORRECT DISK. PRESS CTRL+C TO CANCEL" ${SECONDS}
+    read -p "Which disk should be erased? (i.e. /dev/sdj): " disk
+    echo "WARNING! BLOCK DEVICE: $disk WILL BE ERASED IN 60 SECONDS..."
+    echo "PLEASE MAKE SURE THIS IS THE CORRECT DISK"
+
+    # Print countdown and wait 60 seconds before rebooting
+    for seconds in {60..1}; do
+	    printf "PRESS CTRL+C TO CANCEL (%2d seconds)\r" ${seconds}
         sleep 1
-        SECONDS=$((SECONDS-1))
     done
-    
+
     # Create new partitions and setup for an MBR install
-    parted -a optimal --script ${DISK} \
+    parted --script $disk \
         mklabel msdos \
-        mkpart primary ext2 0GiB 1GiB \
+        mkpart primary ext2 0% 1GiB \
         set 1 boot on \
         mkpart primary linux-swap 1GiB 9GiB \
-        mkpart primary ext4 11GiB 30GiB \
+        mkpart primary ext4 9GiB 30GiB \
         mkpart primary ext4 30GiB 100% >&2
 
     if [ $? -ne 0 ]; then
-        echo "Error! Something went wrong when writing partion"
+        echo "Error! Something went wrong while writing partion"
         echo "Disk is in an unknown state. You are on your own"
         exit 1
     fi
 
+    # Make local names for disk partitions
+    local boot_dev=${disk}1
+    local swap_dev=${disk}2
+    local root_dev=${disk}3
+    local home_dev=${disk}4
+
+    # Create the filesystems
+    mkfs.ext4 -F $boot_dev
+    mkswap -f $swap_dev
+    mkfs.ext4 -F $root_dev
+    mkfs.ext4 -F $home_dev
+
+
     #
-    # Mount disk partitions to folders
+    # Mount the partitions in the proper order
+    #     Root must go first
+    #     then either /boot or /home
     #
-    local BOOT_DEV=${DISK}1
-    local SWAP_DEV=${DISK}2
-    local ROOT_DEV=${DISK}3
-    local HOME_DEV=${DISK}4
-
-    if [ ! $(mount $ROOT_DEV $ROOT) ] &&
-       [ ! $(findmnt -Mn $ROOT) ]; then
-        echo "Error! Cannot mount $ROOT_DEV to $ROOT..." >&2
-        echo "Double check that $ROOT_DEV exists in your system" >&2
+    if [[ ! $(mount $root_dev $ROOT_DIR) && ! $(findmnt -M $ROOT_DIR) ]]; then
+        echo "Error! Cannot mount $root_dev to $ROOT_DIR..." >&2
+        echo "Double check that $root_dev exists in your system" >&2
         exit 1
     fi
 
-    if [ ! $(mount $BOOT_DEV $BOOT) ] &&
-       [ ! $(findmnt -Mn $BOOT) ]; then
-        echo "Error! Cannot mount $BOOT_DEV to $BOOT..." >&2
-        echo "Double check that $BOOT_DEV exists in your system" >&2
+    mkdir $BOOT_DIR
+    mkdir $HOME_DIR 
+
+    if [[ ! $(mount $boot_dev $BOOT_DIR) && ! $(findmnt -M $BOOT_DIR) ]]; then
+        echo "Error! Cannot mount $boot_dev to $BOOT_DIR..." >&2
+        echo "Double check that $boot_dev exists in your system" >&2
         exit 1
     fi
 
-    if [ ! $(mount $HOME_DEV $HOME) ] &&
-       [ ! $(findmnt -Mn $HOME) ]; then
-        echo "Error! Cannot mount $HOME_DEV to $HOME..." >&2
-        echo "Double check that $HOME_DEV exists in your system" >&2
+    if [[ ! $(mount $home_dev $HOME_DIR) && ! $(findmnt -M $HOME_DIR) ]]; then
+        echo "Error! Cannot mount $home_dev to $HOME_DIR..." >&2
+        echo "Double check that $home_dev exists in your system" >&2
         exit 1
     fi
 
-    if [ ! $(swapon $SWAP_DEV) ]; then
-        echo "Error! Invalid SWAP Partition" >&2
+    if [ $(swapon $swap_dev) ]; then
+        echo "Error! Failed to activate SWAP partition. Maybe $swap_dev is not a swap device?" >&2
         exit 1
     fi
     
-    cd $ROOT
+    cd $ROOT_DIR
     
     # If STAGE3 does not exist, get from tftp server
     if [ ! -f $STAGE3 ]; then
-        echo "STAGE3 not in directory, downloading from local tftp..."
+        echo "$STAGE3 not in directory, downloading from local tftp..."
         curl -o $STAGE3 tftp://$TFTP/gentoo-deploy/$FOLDER/$STAGE3 >&2
     fi
 
     # If STAGE3 does not exist, get from the internet
     if [ ! -f $STAGE3 ]; then
-        echo "STAGE3 not in directory, downloading from mirror..."
+        echo "$STAGE3 not in directory, downloading from mirror..."
         wget -N $LOCATION/$FOLDER/$STAGE3 >&2
     fi
 
-    # Extract STAGE3 to $ROOT directory
-    tar xzvf $STAGE3
+    # Extract STAGE3 to $ROOT_DIR directory
+    tar xpvf $STAGE3 --xattrs-include='*.*' --numeric-owner
     if [ $? -ne 0 ]; then
-        echo "File does not exist"
+        echo "File does not exist, maybe it failed to download?"
         exit 1
     fi
 
     # Define CFLAGS and CXXFLAGS
     if [ ! -z "$MAKECFLAGS" ]; then
-        sed -i "/CFLAGS=.*/c\CFLAGS=\"$MAKECFLAGS\"" .$MAKECONF
+        sed -i "s/COMMON_FLAGS=.*/c\COMMON_FLAGS=\"$MAKECFLAGS\"/" "$MAKECONF"
     fi
 
     # Append MAKEOPTS to the make.conf file
-    echo "MAKEOPTS=\"$MAKEOPTS\"" >> .$MAKECONF
+    echo "MAKEOPTS=\"$MAKEOPTS\"" >> "$MAKECONF"
 
     # Configure the MAKEUSE Variable
     MATCH=`cat /proc/cpuinfo | grep -m 1 -o mmx`
@@ -191,7 +187,7 @@ install() {
     fi
 
     # Append the Make.conf file with more make options 
-    sed -i "/USE=.*/c\USE=\"$MAKEUSE\"" .$MAKECONF
+    sed -i "s/USE=.*/c\USE=\"$MAKEUSE\"/" "$MAKECONF"
     echo "PYTHON_TARGETS=\"$MAKEPYTHON\"" >> .$MAKECONF
     echo "INPUT_DEVICES=\"$MAKEINPUTDEVICES\"" >> .$MAKECONF
     echo "VIDEO_CARDS=\"$MAKEVIDEOCARDS\"" >> .$MAKECONF
@@ -199,25 +195,24 @@ install() {
 
     # Select Mirrors
     mirrorselect -s10 -o >> .$MAKECONF 
-    mirrorselect -i -r -o >> .$MAKECONF
 
     # Copy DNS info before chrooting
     cp -L /etc/resolv.conf ./etc/
     # Copy itself into the chroot directory before chrooting
     cp -L -u $SCRIPT ./root/gentoo-deploy.sh
-
+    
+    # Mount important partitions before chrooting
     mount -t proc proc ./proc
     mount --rbind /sys ./sys
     mount --rbind /dev ./dev
+
+    echo "Chrooting..." >&2
+    source /etc/profile
     chroot ./ /bin/bash -c "/root/gentoo-deploy.sh chroot"
 }
 
 chroot_install() {
-    exec 3>&2
-    exec 2> >(tee "$SCRIPT.log" >&2)
-
     # Show visual (chroot) on shell prompt
-    source /etc/profile
     export PS1="(chroot) ${PS1}"
 
     mkdir -p $PACKAGEKEYWORDS
@@ -225,38 +220,95 @@ chroot_install() {
     emerge --sync
     emerge --oneshot portage
 
+    # Choose the portage profile
+    eselect profile list
+    local PROFILE_NUM
+    read -p "Which profile?:" $PROFILE_NUM
+    eselect profile set $PROFILE_NUM
+
+    # Update the @world set
+    emerge --ask=y --verbose --update --deep --newuse @world
+
+    # Set timezone
+    echo "America/Chicago" > /etc/timezone
+
+    # Set locales
+    echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+    locale-gen
+
     env-update && source /etc/profile
+    
+    # Create fstab automatically
+    genfstab -U -p / >> /etc/fstab
+
+    # Install Kernel Sources
+    emerge --ask=y sys-kernel/gentoo-sources
+    emerge --ask=y sys-kernel/genkernel
+    genkernel all
+
+    # Install firmware for special hardware
+    emerge --ask=y sys-kernel/linux-firmware
+
+    # Set the hostname for the machine
+    local HOSTNAME
+    read -p "Enter desired hostname for this machine" ${HOSTNAME}
+    echo "hostname=${HOSTNAME}" >> /etc/conf.d/hostname
+
+    # Install a few helpful packages
+    emerge --ask=y net-misc/networkmanager
+    rc-update add NetworkManager default
+
+    emerge --ask=y app-admin/sysklogd
+    rc-update add sysklogd default
+
+    emerge --ask=y sys-apps/mlocate
+    emerge --ask=y net-wireless/wpa_supplicant
+
+    # Install the bootloader
+    emerge --ask=y sys-boot/grub:2
+    grub-install ${disk}
+
+    # Change root password and create a user account
+    echo "Set the password for the root account:"
+    passwd
+
+    local USERNAME
+    read -p "Set a user account" ${USERNAME}
+    useradd -m -G users, wheel, audio, disk ${USERNAME}
+    echo "Set the password for ${USERNAME} account"
+    passwd ${USERNAME}
+
+    rm stage3-*.tar*
 }
 
 main() {
-
     # Script must be ran as root user
-    if [ "$(id -u)" -ne 0]; then
+    if [ "$(id -u)" -ne 0 ]; then
         echo "Please run as root"
         exit 1
     fi
 
-    # Check for commandline parameter
-    if [ "$1" == "install" ]; then
-        install()
-    elif [ "$1" == "chroot" ]; then
-        chroot_install()
+    # Open stderr and forward errors to a log file
+    exec 3>&2
+    exec 2> >(tee "$SCRIPT.log" >&2)
+
+    # Check for parameter
+    if [ "$1" = "install" ]; then
+        install
+    elif [ "$1" = "chroot" ]; then
+        chroot_install
     else
         echo "Could not understand $1"
         echo "To start installation, please run $SCRIPT install"
-        echo "To continue installation from a previous chroot, please run $SCRIPT chroot"
-        echo "No changes have been made"
         exit 1
     fi
 
     echo "Deployment Complete."
 
     # Print countdown and wait 15 seconds before rebooting
-    SECONDS=15
-    while [ ${SECONDS} -ge 1 ]; do
-        printf "\rMachine will reboot in %2d seconds..." ${SECONDS}
+    for seconds in {15..1}; do
+        printf "Machine will reboot in %2d seconds...\r" ${seconds}
         sleep 1
-        SECONDS=$((SECONDS-1))
     done
 
     reboot
